42f7a7a0833388b9f759abddc0d3743d59400674
dearjate <yijauregui@gmail.com>
2023-12-29 13:02:38 -0700

    i inicio de proyecto

---
+++ conexion.php
@@ -0,0 +1,13 @@
+<?php
+$servername = "localhost"; //ip o url del servidor
+$database = "lisiv"; //nombre de la BD
+$username = "lisiv"; //usuario de la BD
+$password = "Lisiv123@"; //pass del usuario de la BD
+// Create connection
+$dbcon = mysqli_connect($servername, $username, $password, $database);
+// Check connection
+if (!$dbcon) {
+    die("Falló la conexión: " . mysqli_connect_error());
+}
+//echo "Conectado";
+?>
